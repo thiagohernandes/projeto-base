2cf3d7c8398f7c9edd30f9daaf2561f391701e66
Thiago Hernandes Souza <thernans@everis.com>
2019-06-11 09:08:54 -0300

    Conversão de lista

--- src/main/java/com/camel/code/route/R05Bean.java
+++ src/main/java/com/camel/code/route/R05Bean.java
@@ -1,5 +1,6 @@
 package com.camel.code.route;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.camel.Exchange;
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Component;
 
 import com.camel.code.bean.GerarObjetoBean;
 import com.camel.code.domain.Funcionario;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.AnnotationIntrospector.ReferenceProperty.Type;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @Component
 public class R05Bean  extends RouteBuilder {
@@ -32,21 +33,17 @@ public class R05Bean  extends RouteBuilder {
 			.convertBodyTo(String.class)
 			.to(rotaHttp4)
 			.process(new Processor() {
-		            @Override
-		            public void process(Exchange exchange) throws Exception {
-		            	String jsonList = exchange.getIn().getBody(String.class);
-		                log.info("---> Lista antes " + jsonList);
-		                ObjectMapper mapper = new ObjectMapper();
-		                List<Funcionario> lista = mapper
-		                		.readValue(jsonList, new TypeReference<List<Funcionario>>(){});
-		                lista.add((Funcionario)exchange.getProperty(novoObjeto));
-		                log.info("---> Lista depois ");
-		                lista.stream().forEach(i -> {
-		                	System.out.println(i.getNome());
-		                });
-		            }
+	            @Override
+	            public void process(Exchange exchange) throws Exception {
+					Gson gson = new Gson();
+					java.lang.reflect.Type listaFuncionariosType = new TypeToken<ArrayList<Funcionario>>(){}.getType(); 
+					List<Funcionario> listaFuncionarios= gson.fromJson(exchange.getIn().getBody(String.class), listaFuncionariosType);
+	                listaFuncionarios.add((Funcionario)exchange.getProperty(novoObjeto));
+					listaFuncionarios.forEach(i -> System.out.println(i.getNome()));
+	            }
 	        });
 				
 	}
 
 }
+ 
